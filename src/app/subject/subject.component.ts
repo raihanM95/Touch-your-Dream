1516ddb8ae4cd4b749ec0c4e8176273689555fef
raihanM95 <mahmudrony95@gmail.com>
2019-04-08 01:51:20 +0600

    sub info design

--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-subject',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectComponent implements OnInit {
 
+  BarChart=[];
+
   constructor() { }
 
   ngOnInit() {
+    // Bar chart:
+    this.BarChart = new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: ["CSE", "SWE", "MCT", "EEE", "ETE"],
+        datasets: [{
+          label: '',
+          data: [10, 10, 3, 5, 2, ],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)'
+          ],
+          borderWidth: .5
+        }]
+      }, 
+      options: {
+        title:{
+          text:"Subject details",
+          display:true
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+    });
   }
-
 }
